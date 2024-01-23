e9455f8e52319fb3d27f44498513db6a05420646
Venicius Vieira <veniciusvieira90@gmail.com>
2024-01-23 17:06:59 -0300

    Adicionados os arquivos do restful_v2

---
+++ restful_v2/cad_unico/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from cad_unico.models import Beneficiarios
+
+class BeneficiariosSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Beneficiarios
+        fields = '__all__'
+        
+        
