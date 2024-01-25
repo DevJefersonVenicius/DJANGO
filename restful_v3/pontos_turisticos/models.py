6d5becb2bddda79046124a8e65729d9adb8300c1
Venicius Vieira <veniciusvieira90@gmail.com>
2024-01-25 15:30:57 -0300

    Adicionado a api ao restful_v3

--- restful_v3/pontos_turisticos/models.py
+++ restful_v3/pontos_turisticos/models.py
@@ -7,7 +7,7 @@ class PontoTuristico(models.Model):
     comments = models.TextField()
     address = models.CharField(max_length=150)
     photo = models.ImageField(upload_to='pontos_turisticos', null=True, blank=True) # NULL - nulo BLANK - vazio, pode deixar vazio
-    valuation = models.DecimalField(max_digits=2, decimal_places=2)
+    valuation = models.DecimalField(max_digits=4, decimal_places=2)
     cad_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- restful_v3/pontos_turisticos/urls.py
+++ restful_v3/pontos_turisticos/urls.py
@@ -2,8 +2,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from rest_framework import routers
+from .api.viewsets import PontoTuristicoViewSet
+
+router = routers.DefaultRouter()
+router.register(r'pontos_turisticos', PontoTuristicoViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
+    path('api_pontos_turisticos', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
